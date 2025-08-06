ab56aa1f2c10065e23d53d3b4e7ebd06a4c1dbcd
Abubakkar Sajid <abubakkarsajid4@gmail.com>
2025-08-06 10:06:36 +0500

    Initial commit

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,27 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/dbconnect';
 import router from './routes/router';
+import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Use CORS middleware
+app.use(cors());
+
+// Parse JSON bodies
 app.use(express.json());
 
-dotenv.config();
+// Connect to database
 connectDB();
+
 app.get('/', (_req, res) => {
   res.send('Hello from TypeScript Server!');
 });
+
+// Use API routes
 app.use('/api', router);
 
 app.listen(PORT, () => {
